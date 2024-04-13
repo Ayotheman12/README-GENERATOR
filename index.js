4ad63cbb8eab45c00cd20d1f5cff37313e217d01
Ayotheman12 <150096498+Ayotheman12@users.noreply.github.com>
2024-04-13 05:12:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+// DONE: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// DONE: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is the project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a description for the project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Describe how to install the project:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Describe how to use the project:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Provide guidelines on how to contribute to the project:",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Enter any instructions about how to test the project:",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "What license does your project use?",
+        name: "license",
+        choices: [
+            "Apache License 2.0",
+            "GNU GPLv2",
+            "GNU GPLv3",
+            "ISC License",
+            "MIT License",
+            "Unlicense",
+            "(no license)"
+        ]
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }
+];
+
+// DONE: Create a function to write README file
+function writeToFile(fileName, data) {
+    let markDown = generateMarkdown(data);
+    fs.writeFile(fileName, markDown, err => {
+        err ? console.error(err) : console.log("Success!");
+    });
+}
+
+// DONE: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(response => {
+        writeToFile(`${response.title.split(" ").join("_")}_README.md`, response);
+    });
+}
+
+// Function call to initialize app
+init();
